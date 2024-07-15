51b2a45cfec2e02c7249a7694c77293e5401f7ea
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-07-15 12:32:15 +0200

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+	public static void main( String[] args ) {
+		for (int i = 1; i <= 100; i++) {
+			System.out.println("Valore: " + i);
+			System.out.println(checkValue(i));
+		}
+	}
+
+	private static String checkValue(int value) {
+		String result = "";
+
+		result = (value % 5) == 0
+				? "FizzBuzz"
+				: "Buzz";
+
+		return result;
+	}
+
 }
